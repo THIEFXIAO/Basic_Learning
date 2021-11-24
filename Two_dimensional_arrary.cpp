aeb9f6627637f7f55dadeba82e4c02f608fc3c49
男孩 燃烧的 <xdy19981010@outlook.com>
2021-11-24 21:56:11 +0800

    2021.11.24，回顾之前的基础代码，简单整理准备往下继续学习

--- Two_dimensional_arrary.cpp
+++ Two_dimensional_arrary.cpp
@@ -1,26 +1,25 @@
-/*算了个总成绩*/
+/*算了个总成绩,二维数组的简单使用*/
 
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	int score[3][3] = { {100,100,100},{90,50,100},{60,70,80} }, addscore[3] = { 0, 0, 0 };
-	string name[3] = { "张三","李四","王五" };
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			addscore[i] = addscore[i] + score[i][j];
-		}
-	}
-	for (int i = 0; i < 3; i++)
-	{
-		cout << name[i] << "的总成绩为：";
-		cout << addscore[i] << endl;
-
-	}
-
-	system("pause");
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int score[3][3] = { {100,100,100},{90,50,100},{60,70,80} }, addscore[3] = { 0, 0, 0 };
+//	string name[3] = { "张三","李四","王五" };
+//	for (int i = 0; i < 3; i++)
+//	{
+//		for (int j = 0; j < 3; j++)
+//		{
+//			addscore[i] = addscore[i] + score[i][j];
+//		}
+//	}
+//	for (int i = 0; i < 3; i++)
+//	{
+//		cout << name[i] << "的总成绩为：";
+//		cout << addscore[i] << endl;
+//
+//	}
+//	system("pause");
+//	return 0;
+//}
