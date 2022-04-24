e97d8786c8ede3ebda3d7adb0defa089a64b6e6f
男孩 燃烧的 <xdy19981010@outlook.com>
2022-04-24 09:41:00 +0800

    &和==的运算关系一生之敌

--- test.cpp
+++ test.cpp
@@ -1,46 +1,28 @@
-// 统计十进制转16进制有多少个字母
+// 统计二进制中相隔最远的1
 
 #include <iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<unordered_set>
 
 using namespace std;
 
-string inttohex(int n) {
-    string a;
-    char b[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
-    while (n != 0)
-    {
-        a += b[n % 16];
-        n /= 16;
-    }
-    return a;
-}
-
 int main() {
 
-    for (size_t i = 0; i < INT32_MAX; i++)
+    int n = 100000000;
+
+    if ((n & (n - 1)) == 0) return 0;
+    int ans = 0;
+    int last = -1;
+    for (int i = 0; n; i++, n >>= 1)
     {
-        int n = i;
-        int nn = i;
-        int ans = 0, byte = 15, ans1 = 0;
-
-        string s = inttohex(nn);
-        for (size_t i = 0; i < s.size(); i++)
+        if (n  & 1)
         {
-            if (s[i] > '9') ans1++;
+            if (last > -1) ans = max(i - last, ans);
+            last = i;
         }
-
-        while (n != 0)
-        {
-            if ((n & 15) > 9) ans++;
-            n = n >> 4;
-        }
-        if (ans != ans1)
-        {
-            cout << i << endl;
-            cout << ans << "  " << ans1 << endl;
-        }
-        
     }
-    
-    return 0;
+
+    return ans;
 }
